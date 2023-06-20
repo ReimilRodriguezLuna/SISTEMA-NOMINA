d99818669cac9fa0925dc5d132426e5bbc54aad7
Jose Pineda <Jose Pineda@DESKTOP-OEGRDDF>
2023-06-20 19:38:59 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Empleado
+{
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+    public int Edad { get; set; }
+    public char Sexo { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public bool PoseeLicencia { get; set; }
+    public decimal SueldoBruto { get; set; }
+    public decimal SueldoNeto { get; set; }
+    public decimal TSS { get; set; }
+    public decimal ImpuestoRenta { get; set; }
