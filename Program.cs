f4e6a4fc206e8ed245892b44075087430f48c839
Harpokrat3s <133991192+Harpokrat3s@users.noreply.github.com>
2023-06-21 17:56:49 -0400

    Revert "Update Program.cs"
    This reverts commit d5f9568ba2aff4337280bd779313ca94a973d5a0.

--- Program.cs
+++ Program.cs
@@ -1,39 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-
-
-class Puesto
+﻿class Empleado
 {
-    public string Nombre { get; set; }
-    public string Descripcion { get; set; }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        List<Puesto> puestos = new List<Puesto>();
-
-        // Agregar puestos principales a la lista
-        puestos.Add(new Puesto { Nombre = "Gerente de Proyectos", Descripcion = "Responsable de la gestión de proyectos de software" });
-        puestos.Add(new Puesto { Nombre = "Desarrollador de Software", Descripcion = "Responsable del desarrollo y mantenimiento del software" });
-        puestos.Add(new Puesto { Nombre = "Ingeniero de Calidad de Software", Descripcion = "Responsable de garantizar la calidad del software" });
-        puestos.Add(new Puesto { Nombre = "Analista de datos", Descripcion = "Convertir datos en informacion,informacion en ideas e ideas en decisiones comerciales" });
-        puestos.Add(new Puesto { Nombre = "ingeniero en sistema", Descripcion = "Diseño,implementacion evaluacion y mantenimiento de los sistemas informaticos" });
-
-        {
-
-        }
-        // Imprimir todos los puestos en la consola
-        foreach (Puesto puesto in puestos)
-        {
-            Console.WriteLine("Nombre del puesto: " + puesto.Nombre);
-            Console.WriteLine("Descripción del puesto: " + puesto.Descripcion);
-            Console.WriteLine();
-        }
-
-        Console.ReadLine();
-    }
-}
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+    public int Edad { get; set; }
+    public char Sexo { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public bool PoseeLicencia { get; set; }
+    public decimal SueldoBruto { get; set; }
+    public decimal SueldoNeto { get; set; }
+    public decimal TSS { get; set; }
+    public decimal ImpuestoRenta { get; set; }
